942252a4c887743d8d4021ef3212c55540ae8442
nupaT <trash_jack@mail.ru>
2022-12-07 14:06:59 +0300

    add: async request for choose city

--- script/script.js
+++ script/script.js
@@ -1,17 +1,24 @@
-const searchText = document.getElementById("searchText");
+const searchCity = document.getElementById("searchCity");
 const serachButton = document.getElementById("searchButt");
 const stats = document.querySelector("status");
 
-let url =
-  "http://api.openweathermap.org/geo/1.0/direct?q=Vitebsk,BY&appid=9852ba4ce54f5e1ee00dcc5331937ad8";
+// let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},BY&appid=9852ba4ce54f5e1ee00dcc5331937ad8`;
 let url1 =
   "http://api.openweathermap.org/geo/1.0/reverse?lat=55.194419&lon=30.176051&limit=3&appid=9852ba4ce54f5e1ee00dcc5331937ad8";
 
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (text) {
-    console.log(text[0].name);
-    return text;
-  });
+serachButton.addEventListener("click", () => {
+  let url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value},BY&units=metric&lang=ru&appid=9852ba4ce54f5e1ee00dcc5331937ad8`;
+  // let url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity.value},BY&appid=9852ba4ce54f5e1ee00dcc5331937ad8`;
+  getFetch(url);
+});
+
+async function getFetch(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data[0].local_names.ru);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
