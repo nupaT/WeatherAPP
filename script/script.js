889c64474e8d28821cb6559b8a597068d6409f0f
nupaT <trash_jack@mail.ru>
2022-12-09 14:56:05 +0300

    add: feature description cloud

--- script/script.js
+++ script/script.js
@@ -4,8 +4,10 @@ const serachButton = document.getElementById("searchButt");
 const stats = document.querySelector("status");
 
 const currCity = document.getElementById("currCity");
-const tempValue = document.getElementById("tempValue");
 const weatherIco = document.getElementById("weatherIco");
+const tempValue = document.getElementById("tempValue");
+const FeelsTemp = document.getElementById("FeelsTemp");
+const currClouds = document.getElementById("currClouds");
 
 // let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},BY&appid=9852ba4ce54f5e1ee00dcc5331937ad8`;
 // let url1 =
@@ -21,18 +23,22 @@ async function getFetch(url) {
     const response = await fetch(url);
     const data = await response.json();
     const city = await data?.name;
-    const temp = await data?.main?.temp;
     const currWeatherIco = await data?.weather[0]?.icon;
-    addWeatherCard(city, temp, currWeatherIco);
+    const temp = await data?.main?.temp;
+    const feelsTempValue = await data?.main?.feels_like;
+    const currCloudsValue = await data?.weather[0]?.description;
+    addWeatherCard(city, temp, currWeatherIco, feelsTempValue, currCloudsValue);
     console.log(data); //!!
   } catch (err) {
     console.log(err);
   }
 }
 
-function addWeatherCard(city, temp, currWeatherIco) {
+function addWeatherCard(city, temp, currWeatherIco, feelsTempValue, currCloudsValue) {
   currCity.innerHTML = city;
   const weatherUrl = `http://openweathermap.org/img/wn/${currWeatherIco}.png`;
   weatherIco.style.backgroundImage = `url('${weatherUrl}')`;
   tempValue.innerHTML = `${temp} &#176C`;
+  FeelsTemp.innerHTML = `${feelsTempValue} &#176C`;
+  currClouds.innerHTML = `${currCloudsValue}`;
 }
